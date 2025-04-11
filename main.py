cd383f85ff4eef97a6a232bf725e0ff8d67be843
Aziz Kanchwala <akanchanwala@caesars.com>
2025-04-11 17:00:53 +0530

    Add data ingestion support

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
