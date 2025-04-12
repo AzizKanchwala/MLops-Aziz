dbef3b81e9688dfc26a3e783108998037f452a47
Aziz Kanchwala <akanchanwala@caesars.com>
2025-04-12 19:55:53 +0530

    Allow ssh from any IPv4 IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,38 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+  description = "Allow SSH access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 data "aws_instances" "ci_runner" {
   filter {
     name   = "tag:Name"
@@ -19,15 +51,13 @@ resource "aws_instance" "ci_runner" {
 
   ami                    = var.ami_id
   instance_type          = var.instance_type
+  subnet_id              = data.aws_subnet_ids.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
   key_name               = var.key_name
   associate_public_ip_address = true
 
   tags = {
     Name = "ci-runner"
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > ../ansible/ec2_ip.txt"
   }
 }
 
