290f0b4207d1d0a1a166285b1bc8c301fb6bdb7b
Aziz Kanchwala <akanchanwala@caesars.com>
2025-04-12 09:54:59 +0530

    Data validation added

---
+++ src/mlProject/pipeline/stage_02_validation.py
@@ -0,0 +1,30 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_ingestion import DataIngestion
+from mlProject.components.data_validation import DataValiadtion
+from mlProject import logger
+
+STAGE_NAME = "Data Validation Stage"
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        validation_class = DataValiadtion(data_validation_config)
+        result = validation_class.validate_all_columns()
+        if result:
+            logger.info("Schema validation succeeded!")
+        else:
+            logger.error("Schema validation failed!")
+        
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
